6e6884f6b4afdcbc2205516a452e5b5895a45db2
Zephery <1570631036@qq.com>
2017-08-19 17:21:57 +0800

    Initial commit

---
+++ weiboflask.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify, g, render_template, redirect, url_for, session, current_app
+import multi_NB as ada_real
+import sys
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route("/hello/<word>")
+def hello_user(word):
+    temp =ada_real.test(word)
+    return str(temp)
+
+
+if __name__ == '__main__':
+    print(sys.path)
+    app.run()
